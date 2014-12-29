4faa2a14bd9f7e62e48fde38107e71c9749c1b50
Drudoo <drudoo@drudoo.com>
2014-12-29 14:06:12 +0100

    Scatter plot of components against each other

--- MSPR_Exam.m
+++ MSPR_Exam.m
@@ -11,9 +11,23 @@ fclose(fid);
 %Seperate data from classifier.
 data=[Idat{1} Idat{2} Idat{3}];
 
-%Generate prdata
-prdat = prdataset(data, Idat{4}, 'featlab',['r';'g';'b'], 'lablist',['skin   ';'nonskin']);
+% Generate prdata
+prdat = prdataset(data, Idat{4}, 'featlab',['red  ';'green';'blue '], 'lablist',['skin   ';'nonskin']);
 
-%Generate training set and data set with a 80-20% ratio.
+% Generate training set and data set with a 80-20% ratio.
 trainSize = round(length(data)*0.8);
 [prtrain, prtest]=gendat(prdat,trainSize);
+
+%% Plot components in a scatter plot against each other. 
+colors = {'red' 'green' 'blue'};
+for i=2:3,
+    for j=1:i-1,
+        figure
+        scatterd(prtrain(:,[i j]),'legend')
+        title(sprintf('Feature %s vs. %s',colors{i},colors{j}))
+    end
+end
+% Skin (blue +) is more grouped together in all three scenarios then
+% nonskin (red *). Tentative analysis would suggest that blue (+) is
+% skin and the red (*) is nonskin. The red (*) data is more spread out of
+% some clusters seem random whereas blue (+) is grouped. 
