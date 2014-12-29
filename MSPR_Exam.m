f14966e7a1c63989f898b7d172dbb0cb47deebda
Drudoo <drudoo@drudoo.com>
2014-12-29 12:55:26 +0100

    generate prdataset

--- MSPR_Exam.m
+++ MSPR_Exam.m
@@ -1,5 +1,8 @@
 clear all;
 prwaitbar off
 fid = fopen('Skin_NonSkin.txt');
-Idat = textscan(fid, '%f%f%f%f','delimiter',' ');
+Idat = textscan(fid, '%f%f%f%s','delimiter',' ');
 fclose(fid);
+
+data=[Idat{1} Idat{2} Idat{3}];
+prdat=prdataset(data, Idat{4}, 'featlab',['r';'g';'b'], 'lablist',['skin   ';'nonskin'])
